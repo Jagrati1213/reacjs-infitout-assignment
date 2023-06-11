2120b1c43fbe0abbd2ee61e9fd6fc4e56951b96d
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-11 18:24:51 +0530

    jg: add responsive header

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -6,36 +6,43 @@ import { DownOutlined } from "@ant-design/icons";
 import MenuList from "./MenuList";
 import ProfileList from "./ProfileList";
 import { IconContext } from "react-icons/lib";
+import ResponsiveHeader from "./ResponsiveHeader";
 const { Header } = Layout;
 
 function Navbar() {
   return (
-    <Header className="bg-white text-black px-5 border-2  h-[78px] overflow-hidden shadow-md">
-      <Row justify={"space-evenly"} className="h-[73px] items-center flex-wrap">
-        <Col className="mx-3 p-1">
-          <img src={logo} alt="logo" className="w-full object-cover" />
-        </Col>
-
-        <Col span={10}>
-          <MenuList />
-        </Col>
-
-        <Col span={9}>
-          <ProfileList />
-        </Col>
-
-        <Col
-          className="text-sm font-medium flex justify-center items-center cursor-pointer"
-          style={{ lineHeight: "0" }}
+    <>
+      <Header className=" bg-white text-black px-5 border-2  h-[78px] overflow-hidden shadow-md xl:block hidden z-10">
+        <Row
+          justify={"space-between"}
+          className="h-[73px] items-center flex-wrap"
         >
-          <p className="mx-1">Profile </p>
+          <Col className="mx-3 p-1">
+            <img src={logo} alt="logo" className="w-full object-cover" />
+          </Col>
 
-          <IconContext.Provider value={{ size: "20px" }}>
-            <RiArrowDownSLine />
-          </IconContext.Provider>
-        </Col>
-      </Row>
-    </Header>
+          <Col xl={{ span: 8 }}>
+            <MenuList />
+          </Col>
+
+          <Col span={7}>
+            <ProfileList />
+          </Col>
+
+          <Col
+            className="text-sm font-medium flex justify-center items-center cursor-pointer"
+            style={{ lineHeight: "0" }}
+          >
+            <p className="mx-1">Profile </p>
+
+            <IconContext.Provider value={{ size: "20px" }}>
+              <RiArrowDownSLine />
+            </IconContext.Provider>
+          </Col>
+        </Row>
+      </Header>
+      <ResponsiveHeader />
+    </>
   );
 }
 
