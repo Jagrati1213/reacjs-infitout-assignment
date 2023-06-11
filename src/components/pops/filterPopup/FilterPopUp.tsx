30fe2abad9b86fb2e26948da71c5ddf2fc66b99e
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-11 10:30:45 +0530

    jg: remove and add filter component

--- src/components/pops/filterPopup/FilterPopUp.tsx
+++ src/components/pops/filterPopup/FilterPopUp.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Select, Checkbox, Input } from "antd";
+import { Select, Checkbox, Input, InputNumber, Row, Col } from "antd";
 import type { CheckboxChangeEvent } from "antd/es/checkbox";
 import { RxCross2 } from "react-icons/rx";
-import "../mainContainer/style.module.scss";
+import Style from "./Style.module.css";
+import { Value } from "sass";
 
 function FilterPopUp() {
   const cancel = (e: React.SyntheticEvent<EventTarget>): void => {};
   const handleChange = (value: { value: string; label: React.ReactNode }) => {};
+  const onChangeNum = () => {}; //add type for value
   const onChange = (e: CheckboxChangeEvent) => {};
   //   const showModal = () => {
   //     setIsModalOpen(true);
@@ -21,24 +23,34 @@ function FilterPopUp() {
   //   };
 
   return (
-    <section className="w-[280px] p-3">
-      <div className="flex justify-between my-2">
+    <Row className={`${Style.filterPop} w-[280px] p-3`}>
+      <Col className="flex justify-between my-2" span={24}>
         <p className=" font-bold text-sm text-black">Filters</p>
         <span>
           <RxCross2 />
         </span>
-      </div>
+      </Col>
 
-      <div className="my-2">
+      <Col className="my-2" span={24}>
         <p className="my-2 font-normal">LTP range</p>
-        <div className="flex justify-between">
-          <Input className="w-[120px]" />
+        <Row className="flex justify-between">
+          {/* <Input className="w-[120px]" /> */}
+          <InputNumber
+            className="w-[120px]"
+            defaultValue={1200}
+            onChange={onChangeNum}
+          />
           <p className="text-lg"> - </p>
-          <Input className="w-[120px]" />
-        </div>
-      </div>
+          {/* <Input className="w-[120px]" /> */}
+          <InputNumber
+            className="w-[120px]"
+            defaultValue={1200}
+            onChange={onChangeNum}
+          />
+        </Row>
+      </Col>
 
-      <div className="flex justify-between my-2">
+      <Col className="flex justify-between my-2" span={24}>
         <p className="my-2 font-normal">LTP change % by</p>
         <Select
           labelInValue
@@ -56,21 +68,26 @@ function FilterPopUp() {
             },
           ]}
         />
-      </div>
+      </Col>
 
-      <div className="flex justify-between my-2">
+      <Col className="flex justify-between my-2" span={24}>
         <p className="my-2 font-normal">Specific strike</p>
-        <Input className="w-[120px]" />
-      </div>
+        {/* <Input className="w-[120px]" /> */}
+        <InputNumber
+          className="w-[120px]"
+          defaultValue={0}
+          onChange={onChangeNum}
+        />
+      </Col>
 
-      <div className="flex justify-between my-2">
+      <Col className="flex justify-between my-2" span={24}>
         <p className="my-2 font-normal">Display</p>
-        <div className="flex items-center">
+        <Col className="flex items-center">
           <Checkbox onChange={onChange}>Calls</Checkbox>
           <Checkbox onChange={onChange}>Puts</Checkbox>
-        </div>
-      </div>
-    </section>
+        </Col>
+      </Col>
+    </Row>
   );
 }
 
