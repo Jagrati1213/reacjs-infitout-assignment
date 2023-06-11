ecfbd6256e89f7aebcee36415f04034717ec15dd
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-11 18:23:53 +0530

    jg: create responsive header

---
+++ src/components/header/ResponsiveHeader.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Button, Col, Drawer, Row } from "antd";
+import { Header } from "antd/es/layout/layout";
+import logo from "../../image/logo.png";
+import MenuList from "./MenuList";
+import ProfileList from "./ProfileList";
+import { IconContext } from "react-icons/lib";
+import { RiArrowDownSLine } from "react-icons/ri";
+import { MenuFoldOutlined } from "@ant-design/icons";
+import style from "./style.module.scss";
+
+function ResponsiveHeader() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+  return (
+    <section className="xl:hidden block">
+      <Row
+        justify={"space-between"}
+        className="bg-white text-black px-5 border-2  shadow-md h-[73px] items-center flex-wrap"
+      >
+        <Col className="mx-3 p-1">
+          <img src={logo} alt="logo" className="w-full object-cover" />
+        </Col>
+        <Col>
+          <Button onClick={showDrawer}>
+            <MenuFoldOutlined />
+          </Button>
+        </Col>
+      </Row>
+
+      <Drawer
+        title="Track Trades"
+        placement="right"
+        onClose={onClose}
+        open={open}
+        className={`${style.drawerStyle} bg-white z-10 xl:hidden block`}
+      >
+        <Row
+          justify={"space-evenly"}
+          className="flex-wrap flex-col items-start pl-3"
+        >
+          <Col span={24}>
+            <MenuList />
+          </Col>
+
+          <Col span={24}>
+            <ProfileList />
+          </Col>
+
+          <Col className="text-base font-medium flex justify-center items-center cursor-pointer my-4">
+            <p className="mx-1">Profile </p>
+
+            <IconContext.Provider value={{ size: "22px" }}>
+              <RiArrowDownSLine />
+            </IconContext.Provider>
+          </Col>
+        </Row>
+      </Drawer>
+    </section>
+  );
+}
+
+export default ResponsiveHeader;
